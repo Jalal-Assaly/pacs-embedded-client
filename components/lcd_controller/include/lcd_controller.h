2e4175dfd1b1500919086770c44c1fd00b20bc9e
Jalal Assaly <jalal.assaly@gmail.com>
2024-05-12 00:50:40 +0300

    Update LCD pins

--- components/lcd_controller/include/lcd_controller.h
+++ components/lcd_controller/include/lcd_controller.h
@@ -3,13 +3,13 @@
 
 #include    <stdint.h>
 
-#define     LCD_RS        (13)
-#define     LCD_E         (14)
+#define     LCD_RS        (33)
+#define     LCD_E         (25)
 
-#define     LCD_D4        (27)
-#define     LCD_D5        (26)
-#define     LCD_D6        (25)
-#define     LCD_D7        (33)
+#define     LCD_D4        (26)
+#define     LCD_D5        (27)
+#define     LCD_D6        (14)
+#define     LCD_D7        (13)
 
 void lcd_init();
 void lcd_clear();
